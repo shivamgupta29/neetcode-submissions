d3ddb261f26f9353d136d1ee93898a4f0bde1890
shivamgupta29 <157825479+shivamgupta29@users.noreply.github.com>
2026-07-09 13:13:39 +0530

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid, int i, int j){
+        if(i >= grid.size() || i<0 || j>=grid[0].size() || j<0) return;
+        if(grid[i][j] == '0') return;
+        grid[i][j] = '0';
+        dfs(grid, i-1, j);
+        dfs(grid, i+1, j);
+        dfs(grid, i, j-1);
+        dfs(grid, i, j+1);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int count=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j] == '0') continue;
+                dfs(grid, i, j);
+                count++;
+            }
+        }
+        return count;
+    }
+};
