d97577c7c2b07c3746e7a6b86f186e6fcd9a48a5
shivamgupta29 <157825479+shivamgupta29@users.noreply.github.com>
2026-07-22 10:39:30 +0530

    Add: perfect-squares - submission-0

---
+++ Data Structures & Algorithms/perfect-squares/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n+1, INT_MAX);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                dp[i] = min(dp[i], 1+dp[i-j*j]);
+            }
+        }
+        return dp[n];
+    }
+};
